91a61dc006fffcfdf9363f8a49d6725404726d35
André Hortelão <andrehortelao93@gmail.com>
2023-02-07 14:29:12 +0000

    login/login-action

--- PHP/devsbook/models/Auth.php
+++ PHP/devsbook/models/Auth.php
@@ -22,4 +22,29 @@ class Auth {
         exit;
     }
 
+    public function validateLogin($email, $password) {
+        $userDao = new UserDaoMysql($this->pdo);
+
+        $user = $userDao->findByEmail($email);
+        if($user) {
+
+            if(password_verify($password, $user->password)) {
+                $token = md5(time().rand(0, 9999));
+                $_SESSION['token'] = $token;
+                $user->token = $token;
+                $userDao->update($user);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public function emailExists($email) {
+        $userDao = new UserDaoMysql($this->pdo);
+        return $userDao->findByEmail($email) ? true : false;
+    }
+
 }
