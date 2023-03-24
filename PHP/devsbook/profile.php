cc51485b1ae7e216b13f26a21329a726e9c324da
André Hortelão <andrefhortelao@gmail.com>
2023-03-24 16:52:49 +0000

    Update profile.php

--- PHP/devsbook/profile.php
+++ PHP/devsbook/profile.php
@@ -134,15 +134,15 @@ require 'partials/menu.php';
                                 <?php foreach($user->following as $item): ?>
 
                                     <div class="friend-icon">
-                                <a href="<?=$base;?>/profile.php?id=<?=$item->id;?>">
+                                        <a href="<?=$base;?>/profile.php?id=<?=$item->id;?>">
                                     <div class="friend-icon-avatar">
                                         <img src="<?=$base;?>/media/avatars/<?=$item->avatar;?>" />
                                     </div>
                                     <div class="friend-icon-name">
                                     <?=$item->name;?>
                                     </div>
-                                </a>
-                            </div>
+                                    </a>
+                                </div>
 
                                 <?php endforeach; ?>
                             <?php endif; ?>
